6e65de673b011e663281a9381d4091a8afe0f438
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-09-23 20:52:40 -0400

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // For statement that prints 1 through 5
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+
+        }
+
+        // A Do-While statement that prints 1 through 5
+        int j = 1;
+        boolean isReady = false;
+        do {
+            if (j > 5) {
+                break;
+            }
+            System.out.println(j);
+            j++;
+            isReady = (j > 0);
+
+        } while (isReady);
+
+
+        // While statement
+        int number = 0;
+        while (number < 50){
+            number += 5;
+
+            if (number % 25 == 0){
+                continue;
+            }
+
+            System.out.print(number + "_");
+
+        }
+
+
+    }
+
+}
